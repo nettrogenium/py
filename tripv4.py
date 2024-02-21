1a715138768abb162ec7350530d57b1a7f4b913f
Nett <151877350+nettrogenium@users.noreply.github.com>
2024-02-22 00:06:07 +0300

    Update tripv4.py
    extended trip to identify ports

--- tripv4.py
+++ tripv4.py
@@ -1,14 +1,21 @@
-# currently checks only ip w/o port (127.0.0.1:4444 -> False)
-# trip = TRue IP
+#trip = TRue IP
 def trip(IPv4):
+    
+    # replace colons with dot to identify port
+    IPv4 = IPv4.replace(':', '.')
+    
+    # check if dots are correct
     try:
         IPv4 = list(map(int, IPv4.split('.')))
     except ValueError:
         return False
+    
+    # check if port is correct
+    if IPv4[4] < 0 or IPv4[4] > 65535: return False
+    
+    # check if octets are correct
     for i in range(4):
         if IPv4[i] < 0 or IPv4[i] > 255: return False
-        
+            
+    # if IPv4 is correct, return True    
     return True
-    
-print(trip(input()))
-    
