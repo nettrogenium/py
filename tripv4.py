6e81fa585bd2cdbafb9f022ead8fc2dbd43bb113
Nett <151877350+nettrogenium@users.noreply.github.com>
2024-02-23 15:00:00 +0300

    Update tripv4.py
    bug fixes

--- tripv4.py
+++ tripv4.py
@@ -1,5 +1,7 @@
 #trip = TRue IP
 def trip(IPv4):
+    # checks if there are only 3 dots in the ip
+    if IPv4.count('.') < 3 or IPv4.count('.') > 4: return False
     
     # replace colons with dot to identify port
     IPv4 = IPv4.replace(':', '.')
@@ -10,12 +12,15 @@ def trip(IPv4):
     except ValueError:
         return False
     
+    # checks if there are more ports/octets
+    if len(IPv4) > 5: return False
+    
     # check if port is correct
-    if IPv4[4] < 0 or IPv4[4] > 65535: return False
+    if len(IPv4) == 5:
+        if IPv4[4] < 0 or IPv4[4] > 65535: return False
     
     # check if octets are correct
     for i in range(4):
         if IPv4[i] < 0 or IPv4[i] > 255: return False
-            
-    # if IPv4 is correct, return True    
+        
     return True
